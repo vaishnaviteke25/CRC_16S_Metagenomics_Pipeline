5a3952494a460e81c32b22d825958b364b42e6f2
vaishnaviteke25 <vaishnaviteke26@gmail.com>
2026-06-09 01:43:24 +0530

    Create phylogeny.py

---
+++ phylogeny.py
@@ -0,0 +1,16 @@
+import os
+
+print("Building phylogenetic tree...")
+
+cmd = """
+qiime phylogeny align-to-tree-mafft-fasttree \
+--i-sequences rep-seqs.qza \
+--o-alignment aligned-rep-seqs.qza \
+--o-masked-alignment masked-aligned-rep-seqs.qza \
+--o-tree unrooted-tree.qza \
+--o-rooted-tree rooted-tree.qza
+"""
+
+os.system(cmd)
+
+print("Phylogenetic tree generated.")
