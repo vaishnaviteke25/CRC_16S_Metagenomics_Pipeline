07f8333de71dc399621ba1366bb2240de1fd3101
vaishnaviteke25 <vaishnaviteke26@gmail.com>
2026-06-09 01:38:35 +0530

    Update import_data.py

--- import_data.py
+++ import_data.py
@@ -1,1 +1,15 @@
+import os
 
+print("Starting QIIME2 import...")
+
+cmd = """
+qiime tools import \
+--type 'SampleData[PairedEndSequencesWithQuality]' \
+--input-path manifest.tsv \
+--output-path demux-paired.qza \
+--input-format PairedEndFastqManifestPhred33V2
+"""
+
+os.system(cmd)
+
+print("Import completed.")
