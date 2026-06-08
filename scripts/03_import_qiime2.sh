6cf46f7a8d5392f1e2637d04086260cb2a77864f
vaishnaviteke25 <vaishnaviteke26@gmail.com>
2026-06-09 01:35:53 +0530

    Delete scripts/03_import_qiime2.sh

--- scripts/03_import_qiime2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-qiime tools import \
---type 'SampleData[PairedEndSequencesWithQuality]' \
---input-path manifest.tsv \
---output-path demux-paired.qza \
---input-format PairedEndFastqManifestPhred33V2
