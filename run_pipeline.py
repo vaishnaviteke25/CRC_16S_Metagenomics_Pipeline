552a7633931d21d13c33dc6d8e74e9460d1fe00d
vaishnaviteke25 <vaishnaviteke26@gmail.com>
2026-06-11 23:04:41 +0530

    Update run_pipeline.py

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,15 +1,24 @@
-import os
+from import_data import import_data
+from denoise import run_dada2
+from summarize import summarize_data
+from phylogeny import generate_tree
+from taxonomy import taxonomy_analysis
+from diversity import diversity_analysis
+from ancombc import run_ancombc
 
-print("Starting CRC 16S Metagenomics Pipeline")
+def main():
 
-os.system("python download_data.py")
-os.system("python fastqc.py")
-os.system("python import_data.py")
-os.system("python denoise.py")
-os.system("python phylogeny.py")
-os.system("python taxonomy.py")
-os.system("python diversity.py")
-os.system("python ancombc.py")
-os.system("python picrust2.py")
+    print("Starting CRC 16S Metagenomics Pipeline...\n")
 
-print("Pipeline completed successfully.")
+    import_data()
+    run_dada2()
+    summarize_data()
+    generate_tree()
+    taxonomy_analysis()
+    diversity_analysis()
+    run_ancombc()
+
+    print("\nPipeline completed successfully!")
+
+if __name__ == "__main__":
+    main()
