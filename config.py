508cd2fb653cad682ed0a931ad2146a161484808
vaishnaviteke25 <vaishnaviteke26@gmail.com>
2026-06-11 22:59:51 +0530

    Add configuration variables for metadata and classifier

---
+++ config.py
@@ -0,0 +1,5 @@
+METADATA = "/mnt/d/COLON3/raw_data/dada2_out/sample-metadata.tsv"
+
+CLASSIFIER = "/mnt/d/COLON3/databases/silva/silva-138-99-nb-human-stool-weighted-classifier.qza"
+
+SAMPLING_DEPTH = 43000
