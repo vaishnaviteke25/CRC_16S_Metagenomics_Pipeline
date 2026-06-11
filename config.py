c609a7c34ae7ee5f385fc0f97e85e1dcfdb411b3
vaishnaviteke25 <vaishnaviteke26@gmail.com>
2026-06-11 23:53:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-METADATA = "/mnt/d/COLON3/raw_data/dada2_out/sample-metadata.tsv"
+METADATA = "sample-metadata.tsv"
 
-CLASSIFIER = "/mnt/d/COLON3/databases/silva/silva-138-99-nb-human-stool-weighted-classifier.qza"
+CLASSIFIER = "silva-138-classifier.qza"
 
 SAMPLING_DEPTH = 43000
